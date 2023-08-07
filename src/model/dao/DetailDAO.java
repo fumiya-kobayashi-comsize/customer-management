0d8ac0d006afb5f2630b08ba50e11a0eb2d23086
com-takeuchi/ <hinako.takeuchi@comsize.com>
2023-08-07 17:15:22 +0900

    DetailDAO

--- src/model/dao/DetailDAO.java
+++ src/model/dao/DetailDAO.java
@@ -4,8 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.entity.CustomerBean;
+import model.entity.UserBean;
 /**
  * m_customerテーブルのDAOです。
  * @author 竹内
@@ -21,8 +24,8 @@ public class DetailDAO {
 			throws SQLException, ClassNotFoundException{
 
 		String sql ="SELECT t1.customer_id, t1.customer_name, t1.customer_name_kana,"
-				+ " t1.postal_code, t1.address, t2.area_name, t1.contact_person_name,"
-				+ " t1.contact_person_name_kana, t1.contact_person_tel, t3.user_name "
+				+ " t1.postal_code, t1.address, t2.area_name, t1.area_code,  t1.contact_person_name,"
+				+ " t1.contact_person_name_kana, t1.contact_person_tel, t3.user_name, t1.user_id "
 				+ " FROM m_customer t1 JOIN m_area t2 ON t1.area_code = t2.area_code "
 				+ " JOIN m_user t3 ON t1.user_id = t3.user_id "
 				+ " WHERE t1.customer_id =?";
@@ -45,14 +48,62 @@ public class DetailDAO {
 				detailbean.setPostalCode(res.getString("postal_code"));
 				detailbean.setAddress(res.getString("address"));
 				detailbean.setAreaName(res.getString("area_name"));
+				detailbean.setAreaCode(res.getString("area_code"));
 				detailbean.setContactPersonName(res.getString("contact_person_name"));
 				detailbean.setContactPersonNameKana(res.getString("contact_person_name_kana"));
 				detailbean.setContactPersonTel(res.getString("contact_person_tel"));
 				detailbean.setUserName(res.getString("user_Name"));
+				detailbean.setUserId(res.getString("user_id"));
 
 			}
 		}
 		return detailbean;
+	}
+
+	public List<UserBean> userList()
+			throws SQLException, ClassNotFoundException{
+
+		List<UserBean> userList = new ArrayList<UserBean>();
+
+		String sql = "SELECT user_id, user_name FROM m_user";
+
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				ResultSet res = pstmt.executeQuery()) {
+
+			while (res.next()) {
+
+				UserBean userbean = new UserBean();
+				userbean.setUserId(res.getString("user_id"));
+				userbean.setUserName(res.getString("user_name"));
+
+				userList.add(userbean);
+			}
+		}
+		return userList;
+	}
+
+	public List<CustomerBean> areaList()
+			throws SQLException, ClassNotFoundException{
+
+		List<CustomerBean> areaList = new ArrayList<CustomerBean>();
+
+		String sql = "SELECT area_code, area_name FROM m_area";
+
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				ResultSet res = pstmt.executeQuery()) {
+
+			while (res.next()) {
+
+				CustomerBean areabean = new CustomerBean();
+				areabean.setAreaCode(res.getString("area_code"));
+				areabean.setAreaName(res.getString("area_name"));
+
+				areaList.add(areabean);
+			}
+		}
+		return areaList;
 	}
 
 }
