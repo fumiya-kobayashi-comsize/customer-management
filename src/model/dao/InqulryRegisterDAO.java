59ed9581bdb2ebab66ea40d82bdda88ab40b2475
tomoe-yoshida-comsize <tomoe.yoshida@comsize.com>
2023-08-22 17:59:33 +0900

    InqulryRegisterServlet

--- src/model/dao/InqulryRegisterDAO.java
+++ src/model/dao/InqulryRegisterDAO.java
@@ -7,11 +7,12 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.entity.CustomerBean;
 import model.entity.InquiryBean;
 
 
 /**
- * t_inquiryテーブルの問合せ情報を
+ * ～説明～
  * @author 吉田
  */
 
@@ -66,6 +67,7 @@ public class InqulryRegisterDAO {
 	 */
 
 	public int selectId(int customerId) throws SQLException, ClassNotFoundException {
+
 		String sql = "SELECT inquiry_id FROM t_inquiry WHERE customer_id = ?";
 		int inquiryId = 0;
 
@@ -83,6 +85,44 @@ public class InqulryRegisterDAO {
 		}
 		return customerId;
 	}
+
+
+	/**
+	 * カスタマーリストを返します。
+	 * @param なし
+	 * @return customer_id、customer_nameのリスト
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
+	public List<CustomerBean> customerList()
+			throws SQLException, ClassNotFoundException{
+
+		List<CustomerBean> customerList = new ArrayList<CustomerBean>();
+
+		String sql = "SELECT customer_id, customer_name FROM m_customer";
+
+		//DB接続の取得、PreparedStatementの取得、SQLステートメントの実行
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				ResultSet res = pstmt.executeQuery()) {
+
+			//結果の操作
+			while (res.next()) {
+
+				//beanのインスタンス化
+				CustomerBean customerbean = new CustomerBean();
+
+				//beanに値をセット
+				customerbean.setCustomerId(res.getInt("customer_id"));
+				customerbean.setCustomerName(res.getString("customer_name"));
+
+				customerList.add(customerbean);
+			}
+		}
+		return customerList;
+	}
+
+
 
 
 
